9fe90468755133c12d407c95e85fabbeb66c6fb2
Nerios Lamaj <nerioslamaj@macbook-pro.home>
2020-06-26 16:13:31 +0100

    Updating cypress

--- tests/e2e/plugins/index.js
+++ tests/e2e/plugins/index.js
@@ -8,6 +8,7 @@
 
 // /* eslint-disable import/no-extraneous-dependencies, global-require */
 // const webpack = require('@cypress/webpack-preprocessor')
+let percyHealthCheck = require('@percy/cypress/task')
 
 module.exports = (on, config) => {
   // on('file:preprocessor', webpack({
@@ -15,11 +16,11 @@ module.exports = (on, config) => {
   //  watchOptions: {}
   // }))
 
+  on("task", percyHealthCheck);
+
   return Object.assign({}, config, {
     fixturesFolder: 'tests/e2e/fixtures',
     integrationFolder: 'tests/e2e/specs',
-    screenshotsFolder: 'tests/e2e/screenshots',
-    videosFolder: 'tests/e2e/videos',
     supportFile: 'tests/e2e/support/index.js'
   })
 }
