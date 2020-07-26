14b9d597558423596580e2fd88d534c20fbb3cdf
Nerios Lamaj <nerios.lamaj@gmail.com>
2020-07-26 20:08:42 +0200

    Adding and using the process.env file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is the Forex API: <code>{process.env.REACT_APP_FOREX_API}</code>
         </p>
         <a
           className="App-link"
